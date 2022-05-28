f34a4fead1cffb2a725209ff815bb2773f0aa3cb
Marvin <marvin.kaesting@student.hs-anhalt.de>
2022-05-28 17:33:15 +0200

    Add weights to hype mastew fow twaining

--- run_yolo_docker.sh
+++ run_yolo_docker.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env sh
 
 scriptDir=$(pwd)
-datasetsDir=./datasets
+datasetDir=$scriptDir/datasets/
 
-# get datasets - contains confidential keys DO NOT SHARE!
+printf '\n'
+
+# get dataset - contains confidential keys DO NOT SHARE!
 curl -L "https://app.roboflow.com/ds/3JqUkFazKy?key=gO8znJvhOO" > \
-roboflow.zip; unzip -u -o roboflow.zip -d $datasetsDir; rm roboflow.zip
+roboflow.zip; unzip -u -o roboflow.zip -d "$datasetDir"; rm roboflow.zip
 
 curl -L "https://app.roboflow.com/ds/t9mXLVwSTE?key=pbKva5v9LR" > \
-roboflow.zip; unzip -u -o roboflow.zip -d $datasetsDir; rm roboflow.zip
+roboflow.zip; unzip -u -o roboflow.zip -d "$datasetDir"; rm roboflow.zip
 
 curl -L "https://app.roboflow.com/ds/EzlBpUrlE0?key=hufaTQ7eg6" > \
-roboflow.zip; unzip -u -o roboflow.zip -d $datasetsDir; rm roboflow.zip
+roboflow.zip; unzip -u -o roboflow.zip -d "$datasetDir"; rm roboflow.zip
 
 # train in yolov5 docker
 sudo docker run --ipc=host -it --gpus all \
--v "$scriptDir"/datasets:/usr/src/datasets \
+-v "$datasetDir":/usr/src/datasets \
 -v "$scriptDir"/runs:/usr/src/app/runs ultralytics/yolov5:latest \
-bash -c "pip install roboflow && python -m torch.distributed.run --nproc_per_node 2 ./train.py --img 640 --batch-size 64 --epochs 300 --data ../datasets/HiddenObject.yaml --weights yolov5s.pt --device 0,1"
+bash -c "python -m torch.distributed.run --nproc_per_node 2 ./train.py --img 640 --batch-size 64 --epochs 300 --data ../datasets/HiddenObject.yaml --weights yolov5s.pt --device 0,1"
